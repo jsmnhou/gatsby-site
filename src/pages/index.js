bedcd6f3d93a31051d2cf5f20ed66c2139e719f1
jsmnhou <jsmnhou@umich.edu>
2023-05-09 10:37:45 -0700

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm the goat.</p>
+      <StaticImage
+        alt="Choncc"
+        src="../images/Choncc_Lifeguard_Tier_1.png"
+        // src="https://static.wikia.nocookie.net/leagueoflegends/images/f/f0/Featherknight_Pengu_Render.png/revision/latest?cb=20191220210852://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
   )
 }
